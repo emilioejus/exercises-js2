dcdbef16969957777363a993384cc367b2431762
emilioejus <emilioejus@gmail.com>
2021-04-06 14:57:05 +0200

    Se realizo el homework de la semana 3 y existen 2 versiones en el ejercicio 2

--- week-3/Homework/1-alarmclock/alarmclock.js
+++ week-3/Homework/1-alarmclock/alarmclock.js
@@ -1,4 +1,43 @@
-function setAlarm() {}
+// La function se encarga de activar la alarma cuando el tiempo se agota
+let rep = -1;
+function setAlarm() {
+  // se toma el valor agregado en el input
+  clearInterval(rep)
+  let value = document.querySelector("#alarmSet").value;
+  //let value2 = parseInt(value) +1;
+  let change = 1000;
+ // let minI = "00:";
+
+  // por cada intervalo de segundo evalua para modificar el dom
+  rep = setInterval(() => {
+    let time = document.querySelector("#timeRemaining");
+    if(value === 0) {
+      clearInterval(rep)
+      playAlarm()
+    }
+ //   if(value < 10) {
+ //     time.innerHTML = `Time Remaining: ${minI}0${value}`;
+ //   } else if(value >= 10 && value <= 60) {
+ //     time.innerHTML = `Time Remaining: ${minI}${value}`;
+ //   }else if(value > 60) {
+ //     // en esta codición creamos 2 variables y cada variable es un condicional para saber cuando lleva el numero 0 por delante
+ //   }
+    let min = parseInt(value/60) < 10 ? `0${parseInt(value/60)}` : parseInt(value/60);
+    let seg = value % 60 < 10 ? `0${value % 60}` : value % 60
+    time.innerHTML = `Time Remaining: ${min}:${seg}`
+    // por cada intervalo se va restando el valor inicial del input
+    // en este caso cada intervalo es un segundo
+    value = value -1;
+  }, change);
+
+  // se creo setTimeout para controlar el setInterval
+//  setTimeout(() => {
+//    clearInterval(rep)
+//  }, value2  * change);
+  
+  // se utiliza el numero obtenido del input para activar el sonido!!!
+  //setTimeout(()=> playAlarm(),value2 * change  )
+}
 
 // DO NOT EDIT BELOW HERE
 
--- week-3/Homework/2-slideshow/slideshow.js
+++ week-3/Homework/2-slideshow/slideshow.js
@@ -1,1 +1,121 @@
-// Write your code here
+//   <----------version 1 --------->
+var imgs = document.querySelectorAll('#carouselExampleControls .carousel-item > img');
+// imsArray nos cambia el nodeList por un array de los src
+var imsArray = Array.prototype.map.call(imgs, img =>  img.src)
+var img1 = document.querySelector('#img1');
+let index = 0;
+let inter;
+
+// buttons
+var btnAutoF = document.querySelector('#autoForwar');
+btnAutoF.onclick = ()=> {
+    inter = setInterval(() => {
+        forwar()
+    }, 1000);
+} 
+var btnAutoB = document.querySelector('#autoBack');
+btnAutoB.onclick = ()=> {
+    inter = setInterval(() => {
+        back()
+    }, 1000);
+}
+var btnForwar = document.querySelector('#forwar');
+btnForwar.onclick = ()=> {
+    forwar()
+}
+var btnBack =  document.querySelector('#back');
+btnBack.onclick = ()=> {
+    back()
+}
+var btnStop = document.querySelector('#stop');
+btnStop.onclick = ()=> {
+    clearInterval(inter)
+}
+// function
+function forwar() {
+    index ++;
+    if(index === imgs.length ){
+        index = 0
+    }
+    img1.src = imsArray[index]
+}
+function back() {
+    index --;
+    if(index === -1) {
+        index = imgs.length -1
+    }
+    img1.src = imsArray[index]
+}
+
+
+//   <----------version 2 --------->
+
+//let btnAutoB = document.querySelector('#aBack');
+//let btnBack =  document.querySelector('#back');
+//let btnStop = document.querySelector('#stop');
+//let btnFowar = document.querySelector('#forwar');
+//let btnAforwar = document.querySelector('#aForwar');    
+//let img = document.querySelector('img');
+//
+//let img1 = './imgs/img1.jpeg';
+//let img2 = './imgs/img2.jpeg';
+//let img3 = './imgs/img3.jpeg';
+//let img4 = './imgs/img4.jpeg';
+//let img5 = './imgs/img5.jpeg';
+//let img6 = './imgs/img6.jpeg';
+//let img7 = './imgs/img7.jpeg';
+//
+//console.log(img1)
+//
+//let imgs = [img1, img2, img3, img4, img5, img6, img7];
+//let index = 0;
+//let inter = -1;
+//img.src = imgs[index];
+//// boton next
+//btnFowar.onclick = ()=> forwar();
+//// boton back
+//btnBack.onclick = ()=> back();
+//// recorrido delantero
+//btnAforwar.onclick = ()=> {
+//    inter = setInterval(() => {
+//        forwar()
+//    }, 1000);
+//}
+//// recorrido trasero
+//btnAutoB.onclick = ()=> {
+//    inter = setInterval(() => {
+//        back()
+//    }, 1000);
+//}
+//// boton stop
+//btnStop.onclick = ()=> {
+//    clearInterval(inter)
+//}
+//
+//function forwar() {
+//    index ++;
+//    if(index === imgs.length) {
+//        index = 0
+//    }
+//    img.src = imgs[index]
+//    console.log(img.src)
+//}
+//
+//function back() {
+//    index --;
+//    if(index === -1 ) {
+//        index = imgs.length -1
+//    }
+//    console.log(imgs[index])
+//    img.src = imgs[index]
+//    
+//}
+//
+
+
+
+
+
+
+
+
